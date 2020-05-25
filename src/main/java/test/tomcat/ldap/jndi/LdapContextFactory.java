8121479f68625a9576f1b28f8a0b215eeaafe495
Tim Lai <60520268+timlaipega@users.noreply.github.com>
2020-05-25 10:38:32 -0700

    Added some logging to troubleshoot.

--- src/main/java/test/tomcat/ldap/jndi/LdapContextFactory.java
+++ src/main/java/test/tomcat/ldap/jndi/LdapContextFactory.java
@@ -8,30 +8,95 @@ import javax.naming.Reference;
 import javax.naming.ldap.InitialLdapContext;
 import java.util.Hashtable;
 import java.util.Enumeration;
+import java.util.logging.Logger;
 
 public class LdapContextFactory implements ObjectFactory {
 
-    public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment)
-        throws Exception {
+	private static final Logger LOGGER = Logger.getLogger( LdapContextFactory.class.getName() );
 
-        Hashtable<Object, Object> env = new Hashtable<>();
+    // public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment)
+    //     throws Exception {
 
-        Reference reference = (Reference) obj;
+    //     Hashtable<Object, Object> env = new Hashtable<>();
 
-        Enumeration<RefAddr> references = reference.getAll();
+    //     Reference reference = (Reference) obj;
 
-        while (references.hasMoreElements()) {
+    //     Enumeration<RefAddr> references = reference.getAll();
 
-            RefAddr address = references.nextElement();
+    //     while (references.hasMoreElements()) {
 
-            String type = address.getType();
+    //         RefAddr address = references.nextElement();
 
-            String content = (String) address.getContent();
+    //         String type = address.getType();
 
-            env.put(type, content);
-        }
+    //         String content = (String) address.getContent();
 
-        return new InitialLdapContext(env, null);
+    //         env.put(type, content);
+    //     }
 
-    }
+    //     return new InitialLdapContext(env, null);
+
+    // }
+
+	public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment)
+		throws Exception {
+
+		Hashtable<Object, Object> env = new Hashtable<>();
+
+		Reference reference = (Reference) obj;
+
+		Enumeration<RefAddr> references = reference.getAll();
+
+		String providerUrl = "no valid URL";
+
+		while (references.hasMoreElements()) {
+
+			RefAddr address = references.nextElement();
+
+			String type = address.getType();
+
+			String content = (String) address.getContent();
+
+			switch (type) {
+
+				case Context.PROVIDER_URL:
+
+					env.put(Context.PROVIDER_URL, content);
+
+					providerUrl = content;
+
+					break;
+
+				default:
+
+					env.put(type, content);
+
+					break;
+			}
+		}
+
+		InitialLdapContext context = null;
+
+		Object result = null;
+
+			try {
+
+				context = new InitialLdapContext(env, null);
+
+				LOGGER.info("looking up for " + providerUrl);
+
+				result = context.lookup(providerUrl);
+			
+			} finally {
+				
+				if (context != null) {
+					context.close();	
+				}
+			}
+
+		LOGGER.info("Created new LDAP Context");
+
+		return result;
+
+	}
 }
